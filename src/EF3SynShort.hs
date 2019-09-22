ed47a219feedf11f95fe05d60313abc9e071c068
Jack Valinsky <jvalinsky@users.noreply.github.com>
2019-09-22 17:10:28 -0400

    Update EF3SynShort.hs

--- src/EF3SynShort.hs
+++ src/EF3SynShort.hs
@@ -1,21 +1,42 @@
-module EF1synShort where
+module EF3synShort where
 
     import Data.List
     
-    data Sent = Sent NP VP deriving Show
-    data NP   = SNOWWHITE  | ALICE  | DOROTHY | GOLDILOCKS | LITTLEMOOK | ATREYU | Everyone | Someone | NP1 DET CN | NP2 DET RCN deriving Show
-    data DET  = The | Every | Some | No | Most | A deriving Show
-    data CN   = Girl   | Boy   | Princess | Dwarf | Giant | Wizard | Sword | Dagger deriving Show
-    data ADJ  = Fake deriving Show
-    data RCN  = RCN1 CN That VP | RCN2 CN That NP TV | RCN3 ADJ CN deriving Show
-    data That = That deriving Show
-    data VP   = Laughed | Cheered | Shuddered | Smiled | VP1 TV NP  | VP3 AV To INF deriving Show
-    data TV   = Loved   | Admired | Helped | Defeated | Caught deriving Show
-    data DV   = Gave deriving Show
-    data AV   = Hoped | Wanted deriving Show
-    data INF  = Laugh | Sheer | Shudder | INF TINF NP deriving Show
-    data TINF = Love | Admire | Help | Defeat | Catch deriving Show
-    data To   = To deriving Show
+    data Verb = Sing | Laugh | Run | Fight | Push | Play | See deriving Show
+    data ProperNoun = Alice | Bob | Charlie | Steve | Bill | Superman deriving Show
+    data Noun = Proper ProperNoun | Cat | Wizard | Postman | Teacher deriving Show
+    
+
+
 
     
-    -- Nouns have grammatical number, in English something is either singular or plural
+
+{-
+
+Nouns have grammatical number, in English something is either singular or plural
+
+The giants laughed.
+The wizards gave the stone to the giants.
+The boys pushed the piano.
+The boys pushed the pianos.
+The boy pushed the piano.
+The dwarves defeated the giants with the swords.
+The boys admired the princess.
+
+
+The water broke through the dam.
+The bottle was full of water.
+The bottle was full of sand.
+The bottle was full of crickets.
+The ground was covered in sand.
+The ground was covered in crickets.
+
+
+Rats live on no evil star.
+    
+    
+    
+    
+    
+    
+    -}
