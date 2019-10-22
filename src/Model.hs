61fcfb7cf859e6ecd8ccd7cec4bc73ee7f2f6992
Jack Valinsky <jvalinsky@users.noreply.github.com>
2019-10-22 12:43:53 -0400

    predicates

--- src/Model.hs
+++ src/Model.hs
@@ -15,8 +15,9 @@ data Atom = Sword1      | Sword2   | Alice'   | Bob'    | Cyrus'     | Ellie'   
             SnowWhite'  | Tom'     | Uli'     | Victor' | Willie'    | Xena'        |
             Zorba'      | Cup1     | Cup2     | Bottle1 | Bottle2    | The_Genesee' |
             Dress1      | Dress2   | Raft1    | Raft2   | Raft3      | Raft4        |
-            Dorothy'    | Fred'    | Glasses1 | Jeans1  | Whiskers'  | Mittens'     | 
-            Gerald'     | Minnie'  | Mickey'  | Sue'    | The_Rhine' | Dis' | Thorin' deriving (Eq, Show, Bounded, Enum)
+            Dorothy'    | Fred'    | Glasses1 | Ring1   | Whiskers'  | Mittens'     | 
+            Gerald'     | Minnie'  | Mickey'  | Sue'    | The_Rhine' | Dis'         | 
+            Thorin'     | Ring2    | Ring3 deriving (Eq, Show, Bounded, Enum)
 
 atoms :: [Atom]
 atoms = [minBound..maxBound]
@@ -29,7 +30,7 @@ data Entity = Pl' [[Atom]] | Ms' [[Atom]] deriving (Eq, Show)
 -- Lists for OnePlacePreds
 thingList :: [Atom]
 thingList = [Sword1, Sword2, Dagger1, Cup1, Cup2, Bottle1, Bottle2, The_Genesee', 
-              Dress1, Dress2, Raft1, Raft2, Raft3, Raft4, Glasses1, Jeans1]
+              Dress1, Dress2, Raft1, Raft2, Raft3, Raft4, Glasses1, Ring1, Ring2, Ring3]
 
 glassesList :: [Atom]
 glassesList = [Glasses1]
@@ -81,7 +82,7 @@ riverList = [The_Genesee', The_Rhine']
 
 magicalList :: [Atom]
 magicalList = [Penny', Alice', Jim', Linda', Ellie', Victor', Kim', Dis', 
-               Thorin', Tom', Cup1, Sword1]
+               Thorin', Tom', Cup1, Sword1, Ring1]
 
 wizardList :: [Atom]
 wizardList = personList `intersect` magicalList
@@ -105,7 +106,7 @@ youngList = [Ollie', Penny', Stuart', Uli', Willie', Noah', Tom',
              Alice', Ellie', Goldilocks', SnowWhite', Dorothy', Mittens']
 
 oldList :: [Atom]
-oldList = [Sword1, Bottle1, Jeans1] ++ (personList \\ youngList) ++ birdList
+oldList = [Sword1, Bottle1, Ring3, Dress2] ++ (personList \\ youngList) ++ birdList
 
 newList :: [Atom]
 newList = thingList \\ oldList
@@ -123,7 +124,22 @@ womanList :: [Atom]
 womanList = oldList `intersect` femaleList
 
 metalList :: [Atom]
-metalList = [Sword1, Sword2, Dagger1]
+metalList = [Sword1, Sword2, Dagger1, Ring1, Ring2, Ring3]
+
+steelList :: [Atom]
+steelList = [Sword1, Sword2, Dagger1]
+
+goldList :: [Atom]
+goldList = [Ring1, Ring2]
+
+silverList :: [Atom]
+silverList = [Ring3]
+
+ringList :: [Atom]
+ringList = [Ring1, Ring2, Ring3]
+
+dressList :: [Atom]
+dressList = [Dress1, Dress2]
 
 shiny :: [Atom]
 shiny = metalList `intersect` newList
@@ -137,9 +153,46 @@ glassList = bottleList ++ glassesList
 ceramicList :: [Atom]
 ceramicList = cupList
 
+wiseList :: [Atom]
+wiseList = [Linda', Irene', Minnie', Xena', Jim', Victor', Fred']
+
+foolishList :: [Atom]
+foolishList = [Alice', Goldilocks', Ollie', Tom', Zorba', Mickey', SnowWhite']
+
+laughList :: [Atom]
+laughList = [Zorba', Alice', Dorothy', Gerald', Tom', Fred', Thorin']
+
+runList :: [Atom]
+runList = [Xena', Thorin', Jim', Alice'] ++ catList ++ mouseList
+
+walkList :: [Atom]
+walkList = [Linda', Fred', Jim', Penny', Ollie', Tom', Bob', Kim', SnowWhite']
+
+smileList :: [Atom]
+smileList = [Tom', Ollie', Alice', Kim', Uli']
+
+swimList :: [Atom]
+swimList = [Gerald', Whiskers', Mickey', Minnie', Thorin', Ollie', Jim']
 
 
 -- Lists for TwoPlacePreds
+
+
+
+
+{-
+data ADJ = Wise  | Foolish | Bad      | Good       | Young | Old   | Present |
+           Heavy | Rusty   | Clean    | Dirty      | Wet   | Dry   | Dull    |
+           Warm  | Cold    | Magical  | Tall       | Short | Long  | Sharp   | 
+           Sweet | Shiney  | Numerous | Widespread  deriving (Show, Eq)
+
+data VP = VP0 INF | VP1 TV DetP | VP3 DV DetP DetP deriving Show
+
+data INF = Laugh    | Scatter  | Smile | Run  | Walk   | Swim deriving (Show, Eq)
+data TV  = Surround | Build    | Love  | Help | Defeat | Chase | Drink | Be | Have deriving (Show, Eq)
+data DV  = Give deriving (Show, Eq)
+-}
+
 
 {-
 extension :: OnePlacePred -> [Entity]
